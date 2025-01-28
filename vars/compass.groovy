528a6c92cad6e44a38589135f93dc63874e1de7e
Devon Lehman <dlehman@asicentral.com>
2025-01-28 13:50:30 -0500

    test 2

--- vars/compass.groovy
+++ vars/compass.groovy
@@ -1,34 +1,38 @@
-def postJsonToWebhook(compassWebhookUrl, jsonString) {
-    def jsonString =  groovy.json.JsonOutput.toJson(jsonObject)
-
-    println(jsonString)
-    try {
-        def response = httpRequest(
-            httpMode: 'POST',
-            contentType: 'APPLICATION_JSON',
-            requestBody: jsonString,
-            url: compassWebhookUrl
-        )
-        println("Response: ${response}")
-    } catch (Exception e) {
-        println("Failed to send JSON to webhook: ${e.message}")
-        throw e;
-    }
+def callHelloWorld() {
+    bat 'echo hello world'
 }
 
-def generateCompassBuildState(repoName, startDate, compassWebhookUrl, buildState) {
-    def jsonObject = [
-        state: buildState,
-        url: env.BUILD_URL,
-        pipelineId: "${repoName}_${env.BRANCH_NAME}_${env.BUILD_ID}",
-        version: env.BUILD_NUMBER,
-        description: env.JOB_NAME,
-        branchName: env.BRANCH_NAME,
-        startedAt: currentTime,
-        completedAt: null,
-        repoName: repoName
-    ]
+// def postJsonToWebhook(compassWebhookUrl, jsonString) {
+//     def jsonString =  groovy.json.JsonOutput.toJson(jsonObject)
 
-    def jsonString =  groovy.json.JsonOutput.toJson(jsonObject)
-    postJsonToWebhook(jsonString)
-}
+//     println(jsonString)
+//     try {
+//         def response = httpRequest(
+//             httpMode: 'POST',
+//             contentType: 'APPLICATION_JSON',
+//             requestBody: jsonString,
+//             url: compassWebhookUrl
+//         )
+//         println("Response: ${response}")
+//     } catch (Exception e) {
+//         println("Failed to send JSON to webhook: ${e.message}")
+//         throw e;
+//     }
+// }
+
+// def generateCompassBuildState(repoName, startDate, compassWebhookUrl, buildState) {
+//     def jsonObject = [
+//         state: buildState,
+//         url: env.BUILD_URL,
+//         pipelineId: "${repoName}_${env.BRANCH_NAME}_${env.BUILD_ID}",
+//         version: env.BUILD_NUMBER,
+//         description: env.JOB_NAME,
+//         branchName: env.BRANCH_NAME,
+//         startedAt: currentTime,
+//         completedAt: null,
+//         repoName: repoName
+//     ]
+
+//     def jsonString =  groovy.json.JsonOutput.toJson(jsonObject)
+//     postJsonToWebhook(jsonString)
+// }
